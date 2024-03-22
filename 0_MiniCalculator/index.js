92f64b6b603ecb32ecd94b6b451246203136c89f
Asad Ali <theeasadali@gmail.com>
2024-03-22 06:44:29 +0500

    Mini Calculator Project Upload

---
+++ 0_MiniCalculator/index.js
@@ -0,0 +1,24 @@
+import inquirer from 'inquirer';
+const inputTaker = await inquirer.prompt([
+    {
+        message: "Enter First Number: ",
+        type: "number",
+        name: "firstNumber"
+    },
+    {
+        message: "Select an operator from the below list.",
+        type: "list",
+        name: "operator",
+        choices: ["add", "subtract", "multiply", "divide", "remainder"]
+    },
+    {
+        message: "Enter Second Number: ",
+        type: "number",
+        name: "secondNumber"
+    }
+]);
+inputTaker.operator === "add" ? console.log(inputTaker.firstNumber + inputTaker.secondNumber) :
+    inputTaker.operator === "subtract" ? console.log(inputTaker.firstNumber - inputTaker.secondNumber) :
+        inputTaker.operator === "multiply" ? console.log(inputTaker.firstNumber * inputTaker.secondNumber) :
+            inputTaker.operator === "divider" ? console.log(inputTaker.firstNumber / inputTaker.secondNumber) :
+                console.log(inputTaker.firstNumber % inputTaker.secondNumber);
